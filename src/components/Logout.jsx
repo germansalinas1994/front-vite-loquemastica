5c6688cdc7b37c39d98ba43b2441326c9bc42c5d
German Salinas <germansalinas.fce@gmail.com>
2023-10-17 14:30:55 -0300

    cambios

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 
 function Logout() {
+    //cuando me deslogueo borro los datos del carrito de localstorage
+    localStorage.removeItem('carrito');
+
+
     const { logout } = useAuth0()
     logout({ logoutParams: { returnTo: window.location.origin } })
 }
