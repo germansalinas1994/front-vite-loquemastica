ee1a9a643ce57a2a85ce38112f9e26bdb1d90e2c
German Salinas <germansalinas.fce@gmail.com>
2023-10-29 17:34:04 -0300

    cambios de domicilio

--- src/pages/Usuario/Domicilio.jsx
+++ src/pages/Usuario/Domicilio.jsx
@@ -1,6 +1,7 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { useAuth0 } from '@auth0/auth0-react';
+import { Box, Typography } from "@mui/material";
 
 const Domicilios = () => {
 
@@ -9,13 +10,26 @@ const Domicilios = () => {
     const { user } = useAuth0();
     useEffect(() => {
 
-        // getDomicilios();
+        getDomicilios();
 
     }, []);
 
     const getDomicilios = async () => {
         try {
-            const response = await axios.post(apiLocalKey + "/domicilios" , {email : user.email});
+            debugger;
+
+
+
+            const response = await axios.get(apiLocalKey + "/getDomicilios", { params: { email: user.email } });
+            debugger;
+
+            if(response.data.result != null){
+                setDomicilios(response.data.result);
+                console.log(domicilios)
+            }
+
+
+
 
 
         } catch (error) {
@@ -24,9 +38,21 @@ const Domicilios = () => {
         }
     }
 
+
+    if (domicilios.length === 0) {
+        return (
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+                <Typography variant="h5" sx={{ marginBottom: '20px' }}>No se econtraron domicilios</Typography>
+            </Box>
+        );
+    }
     return (
+
+        // vamos a hacer un componente para listado de domicilios, van a ser cards como las del carrito pero con la informacion del domicilio
+
+        //voy a hacer un boton para agregar domicilio, que me lleve a un formulario para agregar domicilio
         <div>
-            <h1>Domicilios</h1>
+            <h1>HAY DOMICILIOS</h1>
         </div>
     )
 }
